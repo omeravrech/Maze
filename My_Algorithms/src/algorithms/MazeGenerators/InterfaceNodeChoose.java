82728da3006e7b597be8f9eba36ca7cefe5f91ba
GIGANET\omera <omera@ceragon.com>
2016-08-25 23:41:54 +0300

    Solution

--- My_Algorithms/src/algorithms/MazeGenerators/InterfaceNodeChoose.java
+++
@@ -1,7 +0,0 @@
-package algorithms.MazeGenerators;
-
-import java.util.ArrayList;
-
-public  interface InterfaceNodeChoose<T> {
-	 public T choose(ArrayList<T> ar); 
-}
--- My_Algorithms/src/algorithms/search/SearchAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package algorithms.search;
-
-import java.util.HashSet;
-
-public interface SearchAlgorithm {
-	public HashSet<State> search (Searchable s);
-	public int getNumberOfNodesEvaluated();
-}
