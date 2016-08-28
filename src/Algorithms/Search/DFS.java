4a2dcc4a874080f6d56b372b24ab370010e57bd4
Bar Malka <Barmalka@10.20.30.46>
2016-08-28 20:38:53 +0300

    Javadoc for DFS

--- src/Algorithms/Search/DFS.java
+++ src/Algorithms/Search/DFS.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
+
+/**
+* <h1>DFS Algorithm!</h1>
+* <br>
+* DFS Algorithm will search deeply in every branch by popping from the queue the<br>
+* state with highest cost.
+*
+* @author  Omer Avrech & Bar Malka
+* @version 1.0
+* @since   27/08/2016 
+* @param1 PriorityQueue close
+*/
+
 public class DFS<T> extends CommonSearcher<T>
 {
 	private PriorityQueue<State<T>> close;
@@ -21,6 +34,18 @@ public class DFS<T> extends CommonSearcher<T>
 		this.close = new PriorityQueue<State<T>>();
 	}
 	
+	
+	/**
+	* <h1>DFS - Search overridden function</h1>
+	* Search function will perform the search according to the DFS
+	* algorithm and will return a <u><i>Solution</u></i>.
+	* @see Solution
+	* @author  Omer Avrech & Bar Malka
+	* @version 1.0
+	* @since   27/08/2016
+	* @return Solution
+	* @param1 Searchable
+	*/
 	@Override
 	public Solution<T> search(Searchable<T> problem)
 	{
